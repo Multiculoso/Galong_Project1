72e4b6be4c2dbfaafaf41cc6994c1f68f21fe9ec
Joelson Galong <joelson.github@gmail.com>
2022-11-24 20:56:24 +0800

    ID Registration

---
+++ Projects/CRUD/idreg/index.php
@@ -0,0 +1,19 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>
+		Home
+	</h2>
+	<p>
+		Please proceed to ID list!
+	</p>
+</div>
+
+<?=template_footer()?>
